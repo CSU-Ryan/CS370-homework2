9050defd699fafb6c43e30640436159eb0b9b6a2
CSU-Ryan <enderbro@colostate.edu>
2026-02-03 15:58:33 -0700

    Implements Coordinator.c

--- src/Coordinator.c
+++ src/Coordinator.c
@@ -1,6 +1,35 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <sys/wait.h>
 
-int main() {
-    return 0;
+int main(const int dividend,
+    const int divisor1,
+    const int divisor2,
+    const int divisor3,
+    const int divisor4)
+{
+    const int[4] divisors = {divisor1, divisor2, divisor3, divisor4};
+
+    for (int i = 0; i < 4; i++) {
+        const int divisor = divisors[i];
+
+        const auto pid = fork();
+        if (pid == 0) {
+            // Code for the Child
+            execlp("/bin/checker.o", "/bin/checker.o", divisor, dividend);
+        }
+        else if (pid > 0) {
+            // Code for the Parent
+            int status;
+            wait(&status);
+
+            printf("Coordinator: child process %d returned %d.",
+                pid, WEXITSTATUS(status));
+        }
+        else {
+            // Failed to fork
+            printf("Coordinator: could not fork child process.");
+            return 1;
+        }
+    }
 }
