74287bb8f660e2a7cddf590a558109dda02e6d1e
CSU-Ryan <enderbro@colostate.edu>
2026-02-03 15:25:24 -0700

    Implements Checker

--- src/Checker.c
+++ src/Checker.c
@@ -1,4 +1,17 @@
+#include <stdio.h>
 
 int check(const int divisor, const int dividend) {
-    return 0;
+    printf("Checker process [%d]: Starting.\n", getpid());
+
+    bool divides = dividend % divisor == 0);
+
+    printf("Checker process [%d]: %d *%S* divisible by %d.\n",
+        getpid(),
+        dividend,
+        (divides) ? "IS" : "IS NOT",
+        divisor;
+        );
+    printf("Checker process [%d]: Returning %d.\n", getpid(), divides);
+
+    return divides;
 }
---
+++ src/Checker.h
@@ -0,0 +1,1 @@
+extern int dividend(int divisor, int dividend);
